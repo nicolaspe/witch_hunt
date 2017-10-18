c4008434c9639fcef8382da3031d4afa9c036d29
Nicolas Peña Escarpentier <npescarpentier@gmail.com>
2017-10-18 13:54:43 -0400

    fire drawn

--- witch_sketch.js
+++ witch_sketch.js
@@ -8,14 +8,18 @@ var portName = '/dev/cu.usbmodem1421';
 var serial;
 
 // game variables
-var button = 0;
-var lastButton = 0;
-var fire_intensity = 0;
-var knob = 200;
-var bg;
-var faces = [];
-var currFace = 0;
-var bodyAnimation;
+let button = 0;
+let lastButton = 0;
+let fire_intensity = 0;
+let fireHue = 30;
+let fireSat = 85;
+let fireBri = 100;
+let knob = 200;
+let bg;
+let faces = [];
+let currFace = 0;
+let bodyAnimation;
+let moveLeft, moveRight;
 
 function preload(){
 	// load background
@@ -38,13 +42,19 @@ function preload(){
 function setup(){
 	createCanvas(860, 280);
 	noStroke();
-	imageMode(CENTER);
 
 	// initialize serial communication
 	serial = new p5.SerialPort();
 	serial.open(portName);
 	serial.on('data', parseData);			// function to read incomming data
 	serial.on('error', serialError);	// function to catch errors
+
+	// define game variables
+	imageMode(CENTER);
+	colorMode(HSB, 360, 100, 100, 100);
+	fireHue = 30;
+	fireSat = 85;
+	fireBri = 100;
 
 	// load and create body sprite animation
 	bodyAnimation.frameDelay = 10;
@@ -53,11 +63,23 @@ function setup(){
 function draw(){
 	background(255);
 	image(bg, width/2, height/2);
+
+	// keyboard control
+	if(moveLeft){
+		knob -= 2;
+	} else if (moveRight) {
+		knob += 2;
+  }
+	knob = constrain(knob, 60, width-60);
+
+	// change abuser
 	if (button == 1 && lastButton == 0){
 		changeAbuser();
 	}
 	lastButton = button;
 
+	// display
+	drawFire();
 	drawBody(knob, height*.6);
 }
 
@@ -76,16 +98,30 @@ function serialError(err){
 	// console.log("ERROR:",err);
 	console.log("Serial communication ERROR");
 }
+
 // key controls
 function keyPressed() {
-  if (keyCode === LEFT_ARROW) {
-    knob -= 10;
-  } else if (keyCode === RIGHT_ARROW) {
-    knob += 10;
-  } else if (' ') {
+	if(keyCode == LEFT_ARROW){
+		moveLeft  = true;
+		moveRight = false;
+	} else if (keyCode == RIGHT_ARROW) {
+		moveLeft  = false;
+		moveRight = true;
+	} else if (key == ' ') {
   	changeAbuser();
   }
-	knob = constrain(knob, 60, width-60);
+}
+function keyReleased() {
+	if(keyCode == LEFT_ARROW){
+		moveLeft  = false;
+	} else if (keyCode == RIGHT_ARROW) {
+		moveRight = false;
+	}
+}
+
+// mouse control
+function mousePressed(){
+	console.log("Mouse pos:", mouseX, " , ", mouseY);
 }
 
 // display the body, centered at (posX, posY)
@@ -103,4 +139,25 @@ function changeAbuser(){
 	if(currFace >= faces.length){
 		currFace = 0;
 	}
+}
+function drawFire(){
+	// change coordinates
+	push();
+	translate(470, 233);
+
+	// FIRE: set color
+	let fireColor = color(fireHue, fireSat, fireBri, 7);
+	noStroke();
+	fill(fireColor);
+	// FIRE: draw overlapping
+	for (let i = 0; i < 18; i++) {
+		let x_dim = 140 + i*14;
+		let y_dim = 250 + i*16;
+		arc(0, 0, x_dim, y_dim, PI+0.02, TWO_PI-0.02);
+	}
+
+	// DARKNESS
+
+	// return coordinates
+	pop();
 }
